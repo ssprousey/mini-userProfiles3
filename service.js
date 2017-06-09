821fa4e5ffb82493c17026caab1270734ea44176
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-06-08 18:33:52 -0600

    finished

--- service.js
+++ service.js
@@ -1,10 +1,26 @@
-angular.module('userProfiles').service('mainService', function($http) {
+angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    console.log(1111111, deferred);
+
+    $http({
         method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
-    })
+        url: 'https://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var users = response.data.data;
+      users.forEach(function(user) {
+        user.last_name = 'Sprouse';
+
+      });
+
+      deferred.resolve(users)
+
+    });
+
+    return deferred.promise;
+
   }
+
 
 });
